b8dbaf0c2288bda1569fb533836dd8f4ab802843
Louis Eppler <louis@eppler.co.uk>
2025-12-26 13:05:26 +0100

    Timetable: Improves UX of station search

--- experiments/timetable/stationSuggestion.js
+++ experiments/timetable/stationSuggestion.js
@@ -17,11 +17,11 @@ function textFieldOnChange(id) {
     //TODO: remove spaces, commas, etc
     //TODO: do this once at start (or even in the data directly)
     stations.forEach(station => {
-        station.stop_name = station.stop_name.toLowerCase();
+        station.stop_name_lower = station.stop_name.toLowerCase();
     });
 
 
-    stations.forEach(station => station.priority = prioritiseStation(input, station.stop_name));
+    stations.forEach(station => station.priority = prioritiseStation(input, station.stop_name_lower));
 
     stations = stations.sort((a,b) => a.stop_name.length-b.stop_name.length)
     stations = stations.sort((a,b) => b.priority - a.priority);
@@ -32,7 +32,7 @@ function textFieldOnChange(id) {
     var htmlString = `
     <ul class="list-group">`
 
-    var possibleStations = stations.filter(x => x.stop_name.includes(input));
+    var possibleStations = stations.filter(x => x.stop_name_lower.includes(input));
 
     console.log(possibleStations);
     
@@ -50,6 +50,12 @@ function textFieldOnChange(id) {
 
 function prioritiseStation(input, stationName) {
     var parts = stationName.split(", ")
+
+    if(stationName.includes("hbf")) {
+        //International Stations (e.g. "Berlin Hbf")
+        if(stationName.startsWith(input)) return 3.5;
+        return 0;
+    }
 
     if(parts.length == 1) {
         //Train station (no area given e.g. "Bern Wankdorf")
@@ -80,5 +86,5 @@ function decode_utf8(s) {
 }
 
 function stationSearchSetup() {
-    stations.push({"stop_id":8503000, stop_name:"HB", display_name:"Zürich HB"})
+    //stations.push({"stop_id":8503000, stop_name:"HB", display_name:"Zürich HB"})
 }
